6e6a87e9b085c8c45f84a010749286f3f5e4b28a
vanya <beseir28@gmail.com>
2024-06-19 09:50:32 +0300

    added slider, profile test

--- src/components/Match.tsx
+++ src/components/Match.tsx
@@ -1,25 +1,42 @@
 import "slick-carousel/slick/slick.css";
-
-
 import "../styles/MatchPage.css"
+
 import Slider from "react-slick";
 import {Profile} from "./Profile.tsx";
+import React from "react";
+
 export const Match = () => {
+    function getRandomColor(): string {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
 
     const settings = {
         arrows: false,
         infinite: true,
-        speed: 500,
+        speed: 400,
         slidesToShow: 1,
         slidesToScroll: 1,
     };
+    const props = () => {
+        return{
+            profileImageURL: "./assets/test.jpg",
+            description: "string",
+            username: "Ivan",
+            bgColor: getRandomColor(),
+        }
+
+    }
+    //  {Array.from(leng, (_, index:number) =>  <Profile key = {index} bgColor={"#3d0a42"}/>)}
     return (
         <Slider {...settings} className={"match-slider"}>
-            <Profile/>
-            <Profile/>
-            <Profile/>
-            <Profile/>
-            <Profile/>
+            {Array.from({ length: 120 }).map((_, index) => (
+                <React.Fragment key={index}>{<Profile data={props()}/>}</React.Fragment>
+            ))}
         </Slider>
     );
 };
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,21 @@
 import "../styles/Profile.css"
-export const Profile = () => {
+import React from "react";
+
+interface Props {
+    data:{
+        profileImageURL: string,
+        description: string,
+        username: string,
+        bgColor: string;
+    }
+
+}
+export const Profile: React.FC<Props> = ({data:{username, bgColor, profileImageURL, description}}) => {
     return (
-        <div className={"profile-card"}>
-            some data
-            some data
-            some data
+        <div className={"profile-card"} style={{backgroundColor:bgColor}} >
+            <img src={profileImageURL} alt={"profile picture"} className={"profile-image"}/>
+            <h2 className={"username-text"}>{username}</h2>
+            <p className={"description-text"} >{description}</p>
         </div>
     );
 };
