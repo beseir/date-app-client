ad0f0557080ac84d01a64dd27758795233c774c9
vanya <beseir28@gmail.com>
2024-06-21 10:20:13 +0300

    like/dislike buttons

---
+++ src/components/BottomBar.tsx
@@ -0,0 +1,12 @@
+import "../styles/Buttons.css"
+export const BottomBar = () => {
+    return (
+        <div className={"bar-container"}>
+            <div className={"bottom-bar"}>
+                <div className={"button-bar dislike"}>dislike</div>
+                <div className={"button-bar like"}>like</div>
+            </div>
+        </div>
+
+    );
+};
